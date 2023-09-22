4d934dbddb33e561a88d70451c52bcaaba2b9ac8
pradeep <thomaspradeep06@gmail.com>
2023-09-22 17:03:19 +0530

    Just added a command

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
-variable aws_region {
+variable "aws_region" {
     type = string
     default = "ap-south-1"
     description = "Mumbai location"
 }
 
-variable bucket_name{
+variable "bucket_name" {
     type = string
     default = "matthews-bucket-091423"
     description = "Creating bucket on 14/09/2023 for learning purpose"
 }
+
+#for testing adding this command
+
