30b21c83eb1dfc4c041c2e2287e2475d177c70fc
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2023-09-30 17:10:47 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("testing")
+# Bug 1: Unused variable
+unused_variable = 42
+
+def main():
+    print("Hello, world!")
+
+# Bug 2: Division by zero
+def divide_by_zero():
+    result = 10 / 0  # This will cause a ZeroDivisionError
+
+if __name__ == "__main__":
+    main()
+    divide_by_zero()
