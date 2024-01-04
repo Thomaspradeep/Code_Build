447d4d65995c4064836f7daf7169756cb1408866
pradeep <thomaspradeep06@gmail.com>
2024-01-04 20:50:47 +0530

    testing

--- glue_database.tf
+++ glue_database.tf
@@ -1,3 +1,7 @@
 resource "aws_glue_catalog_database" "glue_credit_process_database" {
     name = "ddm_credit_process_${var.environment}"  
 }
+
+resource "aws_glue_catalog_database" "glue_credit_process_databases" {
+    name = "credit_process_${var.environment}"  
+}
