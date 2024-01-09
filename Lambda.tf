47157ed6a092d63d024e72f2067576f931027f25
pradeep <thomaspradeep06@gmail.com>
2024-01-09 19:29:51 +0530

    handler updated

--- Lambda.tf
+++ Lambda.tf
@@ -6,10 +6,10 @@ data "archive_file" "lambda" {
 
 resource "aws_lambda_function" "transunion"{
     filename = "accesskey.zip"
-    function_name = "Transunion_Access_Key"
+    function_name = "transunion"
     description = "Transunion Access key and Secret key pair rotation"
     role = var.lambda_role
-    handler = "lambda.lambda_handler"
+    handler = "transunion.lambda_handler"
     timeout = 900
     memory_size = 2048
     runtime = "python3.9"
