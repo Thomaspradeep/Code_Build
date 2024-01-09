2f7b7458f1abbb830a9887890b3bcb2fd517fa0d
pradeep <thomaspradeep06@gmail.com>
2024-01-09 18:29:31 +0530

    role arn modified

--- Lambda.tf
+++ Lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "transunion"{
     filename = "AccessKey.py"
     function_name = "Transunion_Access_Key"
     description = "Transunion Access key and Secret key pair rotation"
-    role = data.aws_iam_role.Admin_role.name
+    role = "arn:aws:iam::941598205732:role/Admin_role"
     handler = "Trans"
     timeout = 900
     memory_size = 2048
