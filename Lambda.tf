16b168e5ed3df311962d7af9abd943b4c731e7c1
pradeep <thomaspradeep06@gmail.com>
2024-01-09 18:18:54 +0530

    rolename updated in lambda function

--- Lambda.tf
+++ Lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "transunion"{
     function_name = "Transunion_Access_Key"
     description = "Transunion Access key and Secret key pair rotation"
-    role = data.aws_iam_role.Admin_role
+    role = data.aws_iam_role.Admin_role.name
     handler = "Trans"
     timeout = 900
     memory_size = 2048
