dc318f615729e8f851f661488385d76f43843ebc
pradeep <thomaspradeep06@gmail.com>
2024-01-09 18:35:38 +0530

    data archive added

--- Lambda.tf
+++ Lambda.tf
@@ -1,5 +1,11 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "AccessKey.py"
+  output_path = "AccessKey.zip"
+}
+
 resource "aws_lambda_function" "transunion"{
-    filename = "AccessKey.py"
+    filename = "AccessKey.zip"
     function_name = "Transunion_Access_Key"
     description = "Transunion Access key and Secret key pair rotation"
     role = "arn:aws:iam::941598205732:role/Admin_role"
