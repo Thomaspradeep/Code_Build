a47850de6dd01f5a069b645b7326a8bdcb1e7f68
pradeep <thomaspradeep06@gmail.com>
2024-01-10 17:29:54 +0530

    file name modified

--- Lambda.tf
+++ Lambda.tf
@@ -1,15 +1,15 @@
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "${path.module}/python/accesskey.py"
-  output_path = "${path.module}/python/accesskey.zip"
+  source_file = "${path.module}/transunion_accesskey_rotation/transunion_accesskey_rotation.py"
+  output_path = "${path.module}/transunion_accesskey_rotation/transunion_accesskey_rotation.zip"
 }
 
 resource "aws_lambda_function" "transunion"{
-    filename = "${path.module}/python/accesskey.zip"
+    filename = "${path.module}/transunion_accesskey_rotation/transunion_accesskey_rotation.zip"
     function_name = "transunion"
     description = "Transunion Access key and Secret key pair rotation"
     role = var.lambda_role
-    handler = "accesskey.lambda_handler"
+    handler = "accestransunion_accesskey_rotationskey.lambda_handler"
     timeout = 900
     memory_size = 2048
     runtime = "python3.9"
