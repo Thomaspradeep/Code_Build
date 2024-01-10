e18bf31d88177177156f25049980db404b14cdb0
pradeep <thomaspradeep06@gmail.com>
2024-01-10 11:03:21 +0530

    glue database default permission changed

--- glue_database.tf
+++ glue_database.tf
@@ -1,5 +1,6 @@
 resource "aws_glue_catalog_database" "glue_credit_process_database" {
-    name = "ddm_credit_process_${var.environment}"  
+    name = "ddm_credit_process_${var.environment}" 
+    create_table_default_permission {}
 }
 
 resource "aws_glue_catalog_database" "glue_credit_process_databases" {
