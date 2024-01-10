9d524f57e49297bc6b64fe3c265249db0aae1d8c
pradeep <thomaspradeep06@gmail.com>
2024-01-10 11:32:11 +0530

    testing#

--- glue_database.tf
+++ glue_database.tf
@@ -1,13 +1,15 @@
 resource "aws_glue_catalog_database" "glue_credit_process_database" {
-    name = "ddm_credit_process_${var.environment}" 
-    create_table_default_permission {
-        permissions = ["SELECT"]
-        principal {
-          data_lake_principal_identifier = "arn:aws:iam::941598205732:role/lakeformation"
-        }
-    }
+    name = "ddm_credit_process_${var.environment}"
 }
 
 resource "aws_glue_catalog_database" "glue_credit_process_databases" {
     name = "credit_process_${var.environment}"  
+}
+
+resource "aws_lakeformation_permissions" "example" {
+  principal   = "arn:aws:iam::941598205732:role/lakeformation"
+  permissions = ["CREATE_TABLE", "ALTER", "DROP"]
+  database {
+    name       = aws_glue_catalog_database.glue_credit_process_database.name
+  }
 }
