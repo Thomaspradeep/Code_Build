813b785c03bac5fac2741912df72e60d6633c3ee
pradeep <thomaspradeep06@gmail.com>
2024-01-10 11:25:45 +0530

    testing#

--- glue_database.tf
+++ glue_database.tf
@@ -3,7 +3,7 @@ resource "aws_glue_catalog_database" "glue_credit_process_database" {
     create_table_default_permission {
         permissions = ["SELECT"]
         principal {
-          data_lake_principal_identifier = 
+          data_lake_principal_identifier = "arn:aws:iam::941598205732:role/lakeformation"
         }
     }
 }
