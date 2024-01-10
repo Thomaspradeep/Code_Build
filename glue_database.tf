6a19d42c80cfa87674a197879f1db80465420d61
pradeep <thomaspradeep06@gmail.com>
2024-01-10 11:14:04 +0530

    testing

--- glue_database.tf
+++ glue_database.tf
@@ -1,9 +1,7 @@
 resource "aws_glue_catalog_database" "glue_credit_process_database" {
     name = "ddm_credit_process_${var.environment}" 
     create_table_default_permission {
-        principal {
-          data_lake_principal_identifier = "Lake Formation"
-        }
+        permissions = ["LakeFormation"]
     }
 }
 
