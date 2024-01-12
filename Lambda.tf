1e2c57ee1d351e6a9a7d22c75aebacf83bd72dc8
pradeep <thomaspradeep06@gmail.com>
2024-01-12 15:36:13 +0530

    new trigger created

--- Lambda.tf
+++ Lambda.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "transunion"{
     filename = "${data.archive_file.lambda.output_path}"
     function_name = "transunion"
     description = "Transunion Access key and Secret key pair rotation"
-    role = var.lambda_role
+    role = aws_iam_role.tranunion_lambda_role.arn
     handler = "transunion_accesskey_rotation.lambda_handler"
     timeout = 900
     memory_size = 2048
@@ -17,4 +17,24 @@ resource "aws_lambda_function" "transunion"{
     tags = {
       name = "Transuion"
     }
+}
+
+resource "aws_cloudwatch_event_rule" "every_five_minutes" {
+    name = "every-five-minutes"
+    description = "Fires every five minutes"
+    schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "check_foo_every_five_minutes" {
+    rule = aws_cloudwatch_event_rule.every_five_minutes.name
+    target_id = "transunion"
+    arn = aws_lambda_function.transunion.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.transunion.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.every_five_minutes.arn
 }
