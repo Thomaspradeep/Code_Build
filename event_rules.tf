150551a4c756ff3c7ef76b40c4ba47dfb25786eb
pradeep <thomaspradeep06@gmail.com>
2024-02-02 16:53:23 +0530

    testing version

--- event_rules.tf
+++ event_rules.tf
@@ -1,19 +1,19 @@
-resource "aws_cloudwatch_event_rule" "glue_job_error" {
-    name = "Glue_Error"
-    description = "Collect Glue error messages in CloudWatch"
-    is_enabled = true
-    event_pattern = <<EOF
-{
-        "source":[
-            "aws.glue"
-        ],
-        "detail-type": [
-            "Glue Job Stage Chagne"
-        ],
-        "detail": {
-            "jobName": ["dlx-ddm-branch-distance","dlx-ddm-sftp-deceased-flag"],
-            "state": ["FAILED, STOPPED", "TIMEOUT"]
-        }
-}
-EOF
-}
+# resource "aws_cloudwatch_event_rule" "glue_job_error" {
+#     name = "Glue_Error"
+#     description = "Collect Glue error messages in CloudWatch"
+#     is_enabled = true
+#     event_pattern = <<EOF
+# {
+#         "source":[
+#             "aws.glue"
+#         ],
+#         "detail-type": [
+#             "Glue Job Stage Chagne"
+#         ],
+#         "detail": {
+#             "jobName": ["dlx-ddm-branch-distance","dlx-ddm-sftp-deceased-flag"],
+#             "state": ["FAILED, STOPPED", "TIMEOUT"]
+#         }
+# }
+# EOF
+# }
