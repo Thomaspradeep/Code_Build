f1f1824a99ea18ccdeba8136f936223e53b80e3a
pradeep <thomaspradeep06@gmail.com>
2024-02-02 16:48:30 +0530

    testing version changed

---
+++ event_rules.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "glue_job_error" {
+    name = "Glue_Error"
+    description = "Collect Glue error messages in CloudWatch"
+    state = "Enabled"
+    event_pattern = <<EOF
+{
+        "source":[
+            "aws.glue"
+        ],
+        "detail-type": [
+            "Glue Job Stage Chagne"
+        ],
+        "detail": {
+            "jobName": ["dlx-ddm-branch-distance","dlx-ddm-sftp-deceased-flag"],
+            "state": ["FAILED, STOPPED", "TIMEOUT"]
+        }
+}
+EOF
+}
