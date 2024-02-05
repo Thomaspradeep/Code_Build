99511acd030c36ef5892e1d27ad79071a4aef6f5
pradeep <thomaspradeep06@gmail.com>
2024-02-05 18:06:01 +0530

    event rule modifed

--- cloud_watch.tf
+++ cloud_watch.tf
@@ -24,9 +24,27 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
 resource "aws_cloudwatch_event_rule" "gateway_check_transunion_optout_lambda_target" {
     name = "dlx-ddm-gateway-check"
     description = "Fires every five minutes"
-    schedule_expression = "rate(30 minutes)"
-    is_enabled = false
+    event_pattern = <<EOF
+{
+  "source": ["aws.s3"],
+  "detail-type": ["Object Created"],
+  "detail": {
+    "bucket": {
+      "name": ["matthews-bucket-091423"]
+    },
+    "object": {
+      "key": [{
+        "prefix": "consumer/transunion/optout/"
+      },
+      {
+        "suffix": ".txt"
+        }]
+    }
+  }
 }
+EOF
+}
+
 
 resource "aws_cloudwatch_event_target" "gateway_check_transunion_optout_lambda_targe"{
     rule = aws_cloudwatch_event_rule.gateway_check_transunion_optout_lambda_target.name
