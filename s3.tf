950e3f17ccbd3183ea25cf2053c08ea82b6f8ad9
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-10 14:04:16 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -28,15 +28,23 @@ resource "aws_s3_bucket_object" "log_bucket"{
     key = "Log/land_bucket"
 }
 
+resource "aws_s3_bucket" "glue_bucket_matthew" {
+    bucket = "log-${var.bucket_name}"
+   versioning{
+    enabled = true
+   }
+    tags = {
+        Env = "Dev"
+    }
+}
 
-
-# module "glue_bucket_matthew"{
-#     for_each = var.clients_list
-#     source = "./modules/client_bucket_directories"
+module "glue_bucket_matthew"{
+     for_each = var.clients_list
+     source = "./modules/client_bucket_directories"
     
-#     bucket_id = aws_s3_bucket.glue_bucket_matthew.id
-#     client_name = each.key
-# }
+     bucket_id = aws_s3_bucket.glue_bucket_matthew.id
+     client_name = each.key
+ }
 # resource "aws_s3_bucket" "aws_glue_databucket"{
 #     bucket = join("-", ["aws","glue","data","bucket"])
 #     versioning{
