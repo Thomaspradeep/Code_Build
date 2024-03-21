7ac954ca62e9724f71d319a827c4d3a99ccd54bb
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-21 15:00:32 +0530

    Update kms.tf

--- kms.tf
+++ kms.tf
@@ -1,9 +1,1 @@
-resource "aws_kms_key" "demo" {
-  description            = "KMS key 1"
-  deletion_window_in_days = 10
-}
 
-resource "aws_kms_alias" "demo" {
-  name          = "alias/demo"
-  target_key_id = aws_kms_key.demo.key_id
-}
