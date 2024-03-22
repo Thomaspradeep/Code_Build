ae6f4c42383f25124d3c7626d05af109d6f8bf71
pradeep <thomaspradeep06@gmail.com>
2024-03-22 13:03:22 +0530

    client_bucket_directories module updated

--- modules/client_bucket_directories/main.tf
+++ modules/client_bucket_directories/main.tf
@@ -4,10 +4,3 @@ resource "aws_s3_bucket_object" "ddm_client" {
     key = "ddm_client/${var.client_name}/" 
     content_type = "application/x-directory"
 }
-
-resource "aws_s3_bucket_object" "ddm_client_process" {
-    bucket = var.bucket_id
-    acl = "private"
-    key = "ddm_client/${var.client_name}/Process/" 
-    content_type = "application/x-directory"
-}
