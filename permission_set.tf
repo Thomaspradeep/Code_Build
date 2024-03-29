0b7fce768bb63ff184413755b8a95e4ff512cfe6
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-29 14:38:14 +0530

    Update permission_set.tf

--- permission_set.tf
+++ permission_set.tf
@@ -1,10 +1,36 @@
-data "aws_ssoadmin_instances" "matthew_account" {
-    provider = aws.mgmt
+provider "aws" {
+  # You should specify your AWS region here
+  region = "us-east-1"
+} 
+
+resource "aws_ssoadmin_permission_set" "example" {
+  name        = "test_set"
+  description = "Your Permission Set Description"
+  instance_arn="arn:aws:sso:::instance/ssoins-72238b70b2eebf44"
+  session_duration = "PT2H"
+
+
+  inline_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "VisualEditor0",
+      "Effect": "Allow",
+      "Action": "s3:ListAllMyBuckets",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+
 }
 
-resource "aws_ssoadmin_permission_set" "client_permission_set" {
-  name             = "Example"
-  description      = "An example"
-  instance_arn     = tolist(data.aws_ssoadmin_instances.example.arns)[0]
-  session_duration = "PT2H"
+resource "aws_ssoadmin_account_assignment" "example" {
+  instance_arn       = aws_ssoadmin_permission_set.example.instance_arn
+  permission_set_arn = aws_ssoadmin_permission_set.example.arn
+  principal_type     = "User"
+  principal_id       = "16uma151"
+  target_type        = "develop"
+  target_id          = "058264506178>"
 }
