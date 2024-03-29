bc4098fb0502808f8cad1877a91f86413b3e0e32
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-29 14:43:04 +0530

    Update permission_set.tf

--- permission_set.tf
+++ permission_set.tf
@@ -3,22 +3,6 @@ resource "aws_ssoadmin_permission_set" "example" {
   description = "Your Permission Set Description"
   instance_arn="arn:aws:sso:::instance/ssoins-72238b70b2eebf44"
   session_duration = "PT2H"
-
-
-  inline_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "VisualEditor0",
-      "Effect": "Allow",
-      "Action": "s3:ListAllMyBuckets",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
-
 }
 
 resource "aws_ssoadmin_account_assignment" "example" {
