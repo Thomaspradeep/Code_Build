0f0a8a61fffaf8971b85975cbc24f7223f615a6f
Thomas pradeep <60232180+Thomaspradeep@users.noreply.github.com>
2024-03-29 15:01:16 +0530

    Update permission_set.tf

--- permission_set.tf
+++ permission_set.tf
@@ -13,7 +13,7 @@ resource "aws_ssoadmin_account_assignment" "example" {
   instance_arn       = aws_ssoadmin_permission_set.example.instance_arn
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
   principal_type     = "USER"
-  principal_id       = "arn:aws:sso:::instance/ssoins-72238b70b2eebf44/16uma151"
+  principal_id       = "c4689408-80b1-7075-4384-06471b80a0fd"
   target_type        = "AWS_ACCOUNT"
   target_id          = "058264506178"
 }
