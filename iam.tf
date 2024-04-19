f79cdd1fc416bf83d5f9b848b48b413ea04d2ec3
pradeep <thomaspradeep06@gmail.com>
2024-04-19 23:16:04 +0530

    iam.tf updated

--- iam.tf
+++ iam.tf
@@ -14,25 +14,6 @@ output "transunion_out"{
      sensitive = true
 }
 
-data "aws_iam_user" "datauser1_user"{
-    user_name = "datauser1"
-}
-resource "aws_iam_access_key" "datauser1_access_key"{
-    user = data.aws_iam_user.datauser1_user.user_name
-}
-
-output "datauser1_access_key"{
-     value = {
-        "key" = aws_iam_access_key.datauser1_access_key.id
-        "secret" = aws_iam_access_key.datauser1_access_key.secret
-     }    
-     sensitive = true
-}
-
 data "aws_iam_role" "Admin_role" {
   name = "Admin_role"
-}
-
-data "aws_iam_user" "user162"{
-    user_name = "1685162"
 }
