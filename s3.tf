84225c36180c919192ee78326088972e920ca30e
pradeep <thomaspradeep06@gmail.com>
2024-04-19 21:59:52 +0530

    bucket updated

--- s3.tf
+++ s3.tf
@@ -1,35 +1,35 @@
-resource "aws_s3_bucket" "log_bucket" {
-    bucket = "log-${var.bucket_name}"
-   versioning{
-    enabled = true
-   }
-    tags = {
-        Env = "Terraform"
-    }
-}
+# resource "aws_s3_bucket" "log_bucket" {
+#     bucket = "log-${var.bucket_name}"
+#    versioning{
+#     enabled = true
+#    }
+#     tags = {
+#         Env = "Terraform"
+#     }
+# }
 
-resource "aws_s3_bucket_object" "log_bucket"{
-    bucket = aws_s3_bucket.log_bucket.id
-    key = "Log/land_bucket"
-}
+# resource "aws_s3_bucket_object" "log_bucket"{
+#     bucket = aws_s3_bucket.log_bucket.id
+#     key = "Log/land_bucket"
+# }
 
-resource "aws_s3_bucket" "CDS_Infra_bucket" {
-    bucket = var.bucket_name
-    logging{
-        target_bucket = aws_s3_bucket.log_bucket.id
-        target_prefix = "Log/land_bucket"
-    }
+# resource "aws_s3_bucket" "CDS_Infra_bucket" {
+#     bucket = var.bucket_name
+#     logging{
+#         target_bucket = aws_s3_bucket.log_bucket.id
+#         target_prefix = "Log/land_bucket"
+#     }
 
-   versioning{
-    enabled = true
-   }
-    tags = {
-        Env = "Terraform"
-    }
-}
+#    versioning{
+#     enabled = true
+#    }
+#     tags = {
+#         Env = "Terraform"
+#     }
+# }
 
 resource "aws_s3_bucket" "glue_bucket_matthew" {
-    bucket = "glue-bucket-matthew01"
+    bucket = "glue-bucket-matthew"
     versioning{
         enabled = true
     }
@@ -38,15 +38,15 @@ resource "aws_s3_bucket" "glue_bucket_matthew" {
      }
  }
 
-resource "aws_s3_bucket" "matthew_bucket_91423" {
-    bucket = "matthew-bucket-91423"
-    versioning{
-        enabled = true
-    }
-     tags = {
-         Env = "Terraform"
-     }
- }
+# resource "aws_s3_bucket" "matthew_bucket_91423" {
+#     bucket = "matthew-bucket-91423"
+#     versioning{
+#         enabled = true
+#     }
+#      tags = {
+#          Env = "Terraform"
+#      }
+#  }
 
  # Objects Modules
 module "glue_bucket_matthew"{
